cfd0d1bf18eb9b29d04b65914c653f76a574bda3
Saman Rajabi <saman.rajabii@gmail.com>
2023-05-13 23:45:30 +0330

    Feat: Add controllers index

---
+++ src/controllers/index.ts
@@ -0,0 +1,5 @@
+import favoriteController from "./favorite.controller";
+import profileController from "./profile.controller";
+import simulatorController from "./simulator.controller";
+
+export { favoriteController, profileController, simulatorController };
