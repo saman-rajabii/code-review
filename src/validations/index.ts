05c41defeb4473b5bd84e06770fe731cc93dc998
Saman Rajabi <saman.rajabii@gmail.com>
2023-05-13 23:51:14 +0330

    Feat: Add validations index

---
+++ src/validations/index.ts
@@ -0,0 +1,5 @@
+import favoriteValidation from "./favorite.validation";
+import profileValidation from "./profile.validation";
+import simulatorValidation from "./simulator.validation";
+
+export { favoriteValidation, profileValidation, simulatorValidation };
