5c16827d5dc0d7145bb700a4056c885889a07a75
Saman Rajabi <saman.rajabii@gmail.com>
2023-05-13 23:48:46 +0330

    Feat: Add models index

---
+++ src/models/index.ts
@@ -0,0 +1,9 @@
+import Favorite from "./Favorite";
+import Profile from "./Profile";
+import Simulator from "./Simulator";
+
+import { IFavorite } from "./Favorite";
+import { ISimulator } from "./Simulator";
+import { IProfile } from "./Profile";
+
+export { Favorite, Profile, Simulator, IFavorite, IProfile, ISimulator };
