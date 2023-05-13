1fedfd58b904b4d8d639c16947c227c509f7c483
Saman Rajabi <saman.rajabii@gmail.com>
2023-05-13 23:50:46 +0330

    Feat: Add profile validation

---
+++ src/validations/profile.validation.ts
@@ -0,0 +1,35 @@
+import joi from "joi";
+import { validate } from "express-validation";
+import { SORT_MODES } from "../enums";
+
+const getProfilesValidation = validate(
+  {
+    query: joi
+      .object({
+        page: joi.number().optional(),
+        pageSize: joi.number().optional(),
+        sort: joi
+          .string()
+          .optional()
+          .valid(...Object.values(SORT_MODES))
+          .insensitive(),
+      })
+      .optional(),
+  },
+  { keyByField: true }
+);
+
+const createProfileValidation = validate(
+  {
+    body: joi
+      .object({
+        email: joi.string().required(),
+        name: joi.string().required(),
+        nickname: joi.string().required(),
+      })
+      .required(),
+  },
+  { keyByField: true }
+);
+
+export default { getProfilesValidation, createProfileValidation };
