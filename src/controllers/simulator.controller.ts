67306ed226ec386e6652566a9583be4de3c13a3f
Saman Rajabi <saman.rajabii@gmail.com>
2023-05-14 18:17:12 +0330

    Perf: Remove condition from get all simulator

--- src/controllers/simulator.controller.ts
+++ src/controllers/simulator.controller.ts
@@ -1,4 +1,3 @@
-import { omit } from "lodash";
 import { NextFunction, Request, Response } from "express";
 import { profileService, simulatorService } from "../services";
 import { MESSAGES, SORT_MODES, STATUS_CODES } from "../enums";
@@ -12,10 +11,7 @@ async function getSimulators(
   try {
     const { pageNumber, pageSize, sort } = request.query;
 
-    const condition = omit(request.query, ["pageNumber", "pageSize", "sort"]);
-
     const [simulators, count] = await simulatorService.getSimulators(
-      condition,
       Number(pageNumber),
       Number(pageSize),
       <SORT_MODES>sort
