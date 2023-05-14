d4c428a77df519cb2baca1795f18cb2f27b635a6
Saman Rajabi <saman.rajabii@gmail.com>
2023-05-14 18:21:11 +0330

    Perf: Remove condition from getFavorites

--- src/services/favorite.service.ts
+++ src/services/favorite.service.ts
@@ -1,17 +1,16 @@
 import { SORT_MODES } from "../enums";
 import { Favorite, IFavorite } from "../models";
 async function getFavorites(
-  condition: any = {},
   pageNumber = 1,
   pageSize = 20,
   sort = SORT_MODES.DSC
 ): Promise<[Partial<IFavorite>[], number]> {
-  const favorites = await Favorite.find(condition)
+  const favorites = await Favorite.find()
     .lean()
     .skip((pageNumber - 1) * pageSize)
     .limit(pageSize)
     .sort({ createdAt: sort == SORT_MODES.ASC ? 1 : -1 });
-  const favoritesCount = await Favorite.countDocuments(condition);
+  const favoritesCount = await Favorite.countDocuments();
 
   return [favorites, favoritesCount];
 }
@@ -19,7 +18,7 @@ async function getFavorites(
 async function getFavoritesByProfileId(
   profile_id: string
 ): Promise<IFavorite[]> {
-  return Favorite.find({ profile_id });
+  return Favorite.find({ profile_id }).lean();
 }
 
 export default {
