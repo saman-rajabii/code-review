e9bf4d94ea4144c12cd102f895a78a9222a3f174
Saman Rajabi <saman.rajabii@gmail.com>
2023-05-15 13:32:51 +0330

    Perf: Change references

--- src/api.ts
+++ src/api.ts
@@ -1,18 +1,21 @@
 import express from "express";
-import { PORT, CORS_ORIGINS } from "./config";
+import config from "./config";
 import cors from "cors";
 import bodyParser from "body-parser";
 import routes from "./routes";
 import { errorHandlerMiddleware } from "./middlewares";
+import { logger } from "./libs";
 import "./db/mongodb";
 
+const { port, corsOrigins } = config;
+
 const app = express();
-app.use(cors({ origin: CORS_ORIGINS }));
+app.use(cors({ origin: corsOrigins }));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use("/api/v1", routes);
 app.use(errorHandlerMiddleware);
 
-app.listen(PORT, () =>
-  console.log(`✅  Ready on port http://localhost:${PORT}`)
+app.listen(port, () =>
+  logger.logInfo(`✅  Ready on port http://localhost:${port}`)
 );
